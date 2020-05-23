47496e4ec11ba6c7cf6e0830ec1411551cee9893
default-007 <brianokola@gmail.com>
2020-05-23 15:39:57 +0300

    Added url to the search results and location

--- photohub/urls.py
+++ photohub/urls.py
@@ -6,9 +6,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns=[
-    url('^$', views.photos, name='photos'),
-    path('search/', views.search_results, name='search_results'),
-    path('location/(/d+)', views.location, name='location')
+    url(r'^$', views.index, name='index'),
+    url(r'^search/', views.search_results, name='search'),
+    url(r'^location/(?P<location>\w+)/', views.image_location, name='location'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
--- photohub/views.py
+++ photohub/views.py
@@ -3,27 +3,26 @@ from django.http import HttpResponse, Http404
 from . models import Image, Location, Category
 
 # Create your views here.
-def photos(request):
-    images = Image.get_all_images()
-    locations = Location.objects.all()
+def index(request):
+    images = Image.objects.all()
+    locations = Location.get_locations()
+    print(locations)
+    return render(request, 'pictures/index.html', {'images': images[::-1], 'locations': locations})
 
-    return render(request,'index.html', {'images':images, 'locations':locations})
+
+def image_location(request, location):
+    images = Image.filter_by_location(location)
+    print(images)
+    return render(request, 'pictures/location.html', {'location_images': images})
+
 
 def search_results(request):
-    if 'location' in request.GET and request.GET["location"]:
-        search_term = request.GET.get("location")
-        searched_image = Image.get_image_by_id(cls,search_term)
-        message = f"{search_term}"
-        
-        return render(request, "search.html", {"message": message, "location":searched_image})
+    if 'imagesearch' in request.GET and request.GET["imagesearch"]:
+        category = request.GET.get("imagesearch")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+        print(searched_images)
+        return render(request, 'pictures/search_results.html', {"message": message, "images": searched_images})
     else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html', {"message":message})
-    
-def location(request,location_id):
-    try:
-        location:location.objects.get(id = location_id)
-    except DoesNotExit:
-        raise Http404()
-    return render(request, "details.html", {"location":location})
-    
+        message = "You haven't searched for any image category"
+        return render(request, 'pictures/search_results.html', {"message": message})
