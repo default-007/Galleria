ce1d67c04296b54651651ad753e8a244483a09d1
default-007 <brianokola@gmail.com>
2020-05-23 13:27:58 +0300

    Create models for the image location and category tables

--- photohub/models.py
+++ photohub/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location_name
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=20),
+    image_description = models.CharField(max_length=50),
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
