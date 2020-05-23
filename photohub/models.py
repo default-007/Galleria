d0487e668707e9d858bf75a3eee68f438a599df2
default-007 <brianokola@gmail.com>
2020-05-23 14:48:01 +0300

    Create save, delete and update location functions and test them

--- photohub/models.py
+++ photohub/models.py
@@ -7,6 +7,19 @@ class Location(models.Model):
     def __str__(self):
         return self.location_name
 
+    def save_location(self):
+        self.save
+
+    def delete_location(self):
+        self.delete
+
+    def update_location(self):
+        self.update
+
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(location_name=value)
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=30)
--- photohub/tests.py
+++ photohub/tests.py
@@ -7,10 +7,30 @@ from .models import Location, Category, Image
 class LocationTestClass(TestCase):
     #Set up method
     def setUp(self):
-        self.kibra = Location(location_name = 'Kibra')
+        self.location = Location(location_name = 'Kibra')
+
+    def tearDown(self):
+        Location.objects.all().delete()
 
 
     # Testing instance
     def test_instances(self):
-        self.assertTrue(isinstance(self.kibra, Location))
-        
+        self.assertTrue(isinstance(self.location, Location))
+
+    #Test for saving method
+    def test_save_location(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertFalse(len(locations)>0)
+
+    #Test for updating location
+    def test_update_location(self):
+        new_location_name = 'Compton'
+        self.location.update_location(self.location.id,new_location_name)
+        updated_location = Location.objects.filter(location_name='Compton')
+        self.assertFalse(len(updated_location)>0)
+
+    def test_delete_location(self):
+        self.location.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location)==0)
